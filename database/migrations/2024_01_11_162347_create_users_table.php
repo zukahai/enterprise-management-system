cd1746d84c4ce08d6b0ec58216a2c9c737e5f216
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-01-11 23:31:51 +0700

    init users table

--- database/migrations/2024_01_11_162347_create_users_table.php
+++ database/migrations/2024_01_11_162347_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('name')->nullable();
+            $table->string('email')->nullable();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
