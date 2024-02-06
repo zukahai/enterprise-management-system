9707e91779602104a15823d20e0bb1b87a170f52
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-02-07 05:36:54 +0700

    style: change language flatpickr

--- public/webhtml/assets/custom-js/main.js
+++ public/webhtml/assets/custom-js/main.js
@@ -1,9 +1,53 @@
 var rangePickr = $('.dt-date'),
-    dateFormat = 'YYYY/MM/DD';
+  dateFormat = 'YYYY/MM/DD';
 
-  if (rangePickr.length) {
-    rangePickr.flatpickr({
-      // enableTime: true,
-      dateFormat: 'd/m/Y',
-    });
-  }
+if (rangePickr.length) {
+  rangePickr.flatpickr({
+    // enableTime: true,
+    dateFormat: 'd/m/Y',
+    locale: {
+      weekdays: {
+        shorthand: ['CN', 'T2', 'T3', 'T4', 'T5', 'T6', 'T7'],
+        longhand: [
+          'Chủ Nhật',
+          'Thứ Hai',
+          'Thứ Ba',
+          'Thứ Tư',
+          'Thứ Năm',
+          'Thứ Sáu',
+          'Thứ Bảy',
+        ],
+      },
+      months: {
+        shorthand: [
+          'Th01',
+          'Th02',
+          'Th03',
+          'Th04',
+          'Th05',
+          'Th06',
+          'Th07',
+          'Th08',
+          'Th09',
+          'Th10',
+          'Th11',
+          'Th12',
+        ],
+        longhand: [
+          'Tháng 1',
+          'Tháng 2',
+          'Tháng 3',
+          'Tháng 4',
+          'Tháng 5',
+          'Tháng 6',
+          'Tháng 7',
+          'Tháng 8',
+          'Tháng 9',
+          'Tháng 10',
+          'Tháng 11',
+          'Tháng 12',
+        ],
+      },
+    },
+  });
+}
