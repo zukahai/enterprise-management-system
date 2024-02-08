411349503ffcb2020f846e10b754bac54f47d52d
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-02-08 17:37:19 +0700

    feat: add SoftDeletes to banks table

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Bank extends Model
 {
     protected $guarded = [];
     use HasFactory;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 }
