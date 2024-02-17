21c2aed59bcb4da6be53bac56d96f085f6ca6097
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-02-17 10:24:56 +0700

    feat: init activity_log

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Bank extends Model
 {
     protected $guarded = [];
     use HasFactory;
     use SoftDeletes;
+    // use LogsActivity;
 
     protected $dates = ['deleted_at'];
 }
