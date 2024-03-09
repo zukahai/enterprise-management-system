06d4a4c8e9333db589595585ed20a25be6690859
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2024-03-09 08:07:52 +0700

    style: update link profile

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -643,7 +643,7 @@
                                     </a>
                                     <ul class="dropdown-menu dropdown-menu-end">
                                         <li>
-                                            <a class="dropdown-item" href="pages-account-settings-account.html">
+                                            <a class="dropdown-item" href="{{route('profile.user', ["username"=>auth()->user()->username])}}">
                                                 <div class="d-flex">
                                                     <div class="flex-shrink-0 me-3">
                                                         <div class="avatar avatar-online">
